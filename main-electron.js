38b9838efc616dad86afc42c96368d719c20d844
koyzxc <jerichopalen11@gmail.com>
2025-11-01 22:44:47 +0800

    Add Electron + Vite 3D Solar System Simulator

---
+++ main-electron.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  // 🟢 Load Vite's dev server (make sure `npm run dev` is running)
+  win.loadURL('http://localhost:5173/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
